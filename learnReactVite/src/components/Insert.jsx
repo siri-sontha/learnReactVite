7b2df6c272d0aebe928e73861208a60652dd9f02
siri-sontha <siri.bly.2005@gmail.com>
2025-06-15 16:04:33 +0530

    completed

--- learnReactVite/src/components/Insert.jsx
+++ learnReactVite/src/components/Insert.jsx
@@ -5,9 +5,7 @@ import Task from './Task.jsx'
 export default function Insert() {
 
     const inputRef = useRef()
-
     const [tasks, setTasks] = useState([])
-    
     const [id, setId] = useState(0)
 
     const add = () => {
@@ -15,19 +13,32 @@ export default function Insert() {
         if (inputText) {
             const newId = id + 1
             setId(newId)
-            const newTask ={
-                id : newId,
-                text: inputText
+            const newTask = {
+                id: newId,
+                text: inputText,
+                isComplete: false,
             }
             setTasks([...tasks, newTask])
+            console.log('new task added with id ' + newId)
             inputRef.current.value = ""
         }
     }
 
+    const del = (taskId) => {
+        const updatedTasks = tasks.filter(task => task.id !== taskId)
+        setTasks(updatedTasks)
+        console.log("Deleted task with id:", taskId)
+    }
+
+    const finish = (taskId) => {
+        const updatedTasks = tasks.map(task => task.id === taskId ? { ...task, isComplete: !task.isComplete } : task)
+        setTasks(updatedTasks)
+    }
+
     return (
         <>
-             {tasks.map(task => (
-          <Task key={task.id} id={task.id} task={task.text} />
+            {tasks.map(task => (
+                <Task key={task.id} id={task.id} task={task} onDelete={() => del(task.id)} finish={() => finish(task.id)} />
             ))}
             <div className="Insert-div">
                 <input ref={inputRef} type="text" placeholder="Add Your Task" />
--- learnReactVite/src/components/Task.jsx
+++ learnReactVite/src/components/Task.jsx
@@ -1,16 +1,15 @@
 import { useRef, useEffect } from "react"
 import trash from "../assets/trash-solid.svg" 
-import Insert from './Insert.jsx'
 
-export default function Task({task = "Add your task here", id}){
+export default function Task({task = "Add your task here", id, onDelete, finish}){
 
     return(
         <>
             <div className="Task-div">
-                <p ref={currentIdRef}>{id}.</p>
-                <input type="checkbox" className="Task-checkbox"></input>
-                <p>{task}</p>
-                <img src={trash} alt="delete" onClick={del} />
+                <p>{id}.</p>
+                <input type="checkbox" className="Task-checkbox" checked={task.isComplete} onChange={finish}></input>
+                <p style={{textDecoration: task.isComplete ? "line-through" : "none"}}>{task.text}</p>
+                <img src={trash} onClick={onDelete}/>
             </div>
         </>
     )
